d0a62be56ddd640298cd60a5265a8d53477bc4c5
Francisco Medel Molinero <73590404+fmedel2000@users.noreply.github.com>
2021-01-16 00:04:40 +0100

    Add files via upload

---
+++ AjedrezGenericidad/src/juego/modelo/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Paquete con los distintos modelos "basados en la realidad"
+ * del juego.
+ *
+ * @author Francisco Medel Molinero
+ * @since JDK 11
+ * @version 2.0
+ */
+package juego.modelo;
